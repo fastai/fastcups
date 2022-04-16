5dfbc6384d87fd2c2f679ec8ef16bfc44ecc5839
Radek Osmulski <rosmulski@gmail.com>
2022-04-17 00:18:10 +1000

    make progress on student side

--- 3cups.py
+++ 3cups.py
@@ -1,9 +1,31 @@
 from flask import Flask
 from flask import render_template
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
-print(__name__)
+socketio = SocketIO(app)
+
+students_count = 0
 
 @app.route("/")
 def hello_world():
     return render_template('student.html')
+
+@socketio.on('connect')
+def handle_student_connect():
+    global students_count
+    students_count += 1
+    print(students_count)
+
+@socketio.on('disconnect')
+def handle_student_disconnect():
+    global students_count
+    students_count -= 1
+    print(students_count)
+
+@socketio.on('color_change')
+def handle_message(data):
+    global counter
+    print(data)
+
+socketio.run(app, debug=True)
