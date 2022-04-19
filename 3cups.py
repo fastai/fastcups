44cff19d4c625d4d45df147cf17de8722599f3f8
Radek Osmulski <rosmulski@gmail.com>
2022-04-19 17:58:23 +1000

    use a cookie for student identification

--- 3cups.py
+++ 3cups.py
@@ -1,36 +1,54 @@
-from flask import Flask
-from flask import render_template, request, current_app
-from flask_socketio import SocketIO, emit, send, join_room, leave_room
+from flask import Flask, render_template, request, make_response
+from flask_socketio import SocketIO, emit
+import random, string, collections
 
 app = Flask(__name__)
 socketio = SocketIO(app)
-students,student2color = set(),dict()
+sid2student, student2color  = dict(), collections.defaultdict(lambda: 'inactive')
 
 @app.route("/")
-def student_interface(): return render_template('student.html')
+def student_interface():
+    student_id = request.cookies.get('student_id')
+    if student_id: # a student opened a new tab
+        emit('close_old_tabs', student_id, broadcast=True, namespace='/')
+    else: # a new student
+        student_id = random_string()
+    response = make_response(render_template('student.html', cup_state=student2color[student_id]))
+    response.set_cookie('student_id', student_id)
+    return response
 
-@socketio.on('new_student')
-def new_student(): students.add(request.sid)
+@socketio.on('get_cup_color')
+def get_cup_color_for_student():
+    return student2color[request.cookies['student_id']]
 
-def student_count(): return len(students)
-def active_student_count(): return len(student2color)
+@socketio.on('register_student')
+def register_student():
+    sid2student[request.sid] = request.cookies['student_id']
+
+def student_count(): return len(sid2student)
+def connected_student2color():
+    return {k: v for k, v in student2color.items() if k in sid2student.values()}
+def active_student_count(): return sum(isinstance(s, str) for s in connected_student2color().values() if s != 'inactive')
 
 def color_fraction():
-    return {color: sum(c==color for c in student2color.values())/(len(student2color) or 1)
+    return {color: sum(c==color for c in connected_student2color().values())/(active_student_count() or 1)
             for color in ['green', 'yellow', 'red']}
 
 @app.route("/teacher")
-def teacher_interface(): return render_template( 'teacher.html', student_count=student_count(),
+def teacher_interface():
+    return render_template( 'teacher.html', student_count=student_count(),
         active_student_count=active_student_count(), color2frac=color_fraction())
 
 @socketio.on('disconnect')
 def handle_disconnect():
-    # we don't know who is disconnecting; might be a student or a teacher, hence discard
-    students.discard(request.sid)
-    student2color.pop(request.sid, None)
+    sid2student.pop(request.sid)
 
 @socketio.on('color_change')
-def handle_message(data): student2color[request.sid] = data['new_color']
+def handle_color_change(new_color): student2color[request.cookies['student_id']] = new_color
+
+def random_string(length=12):
+    letters = string.ascii_letters
+    return ''.join(random.choice(letters) for i in range(length))
 
 socketio.run(app, debug=True)
 
--- README.md
+++ README.md
@@ -9,3 +9,10 @@ In production increase the open file limit by running `ulimit -n 1000000` and ve
 Execute `python 3cups.py` in the root of the repository.
 
 Open a couple of tabs navigating to localhost:5000 and one navigating to localhost:5000/teacher. Press a couple of the "buttons" at the bottom of the screen and see what happens :)
+
+*Important:* Do make sure to restart the server before every class! Otherwise, there exist (at least) two scenarios that otherwise will not be handled and will result in incorrect reporting on active counts:
+
+* student keeping their tab open indefinitely and not showing up for subsequent class but being connected to the Internet at class time
+* student powering off their laptop or losing Internet connection and us never receiving the disconnect event
+
+If we ever put any auth on the teacher interface, we might handle this more gratiously by adding a button to "reset class" which would clean the server state, but probably for now restarting the server is the way to go.
