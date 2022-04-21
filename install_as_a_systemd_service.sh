87d76b8b1775cf9bc47d3d22450368c5014a6355
Radek Osmulski <rosmulski@gmail.com>
2022-04-21 23:53:31 +1000

    add rudimentary install script

--- install_as_a_systemd_service.sh
+++ install_as_a_systemd_service.sh
@@ -1,4 +1,6 @@
-#pip install -r requirements.txt
+pip install -r requirements.txt
+
+ulimit -n 100000
 
 mkdir -p ~/.config/systemd/user/
 
@@ -19,3 +21,19 @@ EOF
 
 systemctl --user enable fastcups
 sudo loginctl enable-linger $USER
+
+sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo tee /etc/apt/trusted.gpg.d/caddy-stable.asc
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+sudo apt update
+sudo apt install caddy
+
+sudo tee /etc/caddy/Caddyfile > /dev/null <<EOF
+http://199.38.217.21
+
+abort /favicon.ico
+reverse_proxy 127.0.0.1:5000
+EOF
+
+sudo HOME=~caddy caddy trust
+sudo ufw allow https
